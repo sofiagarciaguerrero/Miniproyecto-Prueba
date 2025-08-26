65b16de8d0e119858b4f6eef97ba926284feed58
sofiagarciaguerrero <sgarciaguerrero2025@gmail.com>
2025-08-26 19:28:59 +0200

    iniciando

---
+++ crearestructuracarpetas.bat
@@ -0,0 +1,11 @@
+@echo off
+REM Script para crear estructura de proyecto
+
+mkdir data
+mkdir notebooks
+mkdir src
+mkdir results
+mkdir docs
+
+echo Estructura de carpetas creada correctamente.
+pause
